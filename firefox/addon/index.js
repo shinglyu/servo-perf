6719cfcb6e8d5ba215f15a686461bbdd444896b1
Adrian Utrilla <adrianutrilla@gmail.com>
2016-05-18 12:16:58 +0200

    Added firefox addon for performance monitoring

---
+++ firefox/addon/index.js
@@ -0,0 +1,9 @@
+var self = require("sdk/self");
+var pageMod = require("sdk/page-mod");
+
+pageMod.PageMod({
+  include: "*",
+  contentScriptFile: self.data.url('perf.js'),
+  attachTo: ["top"],
+  contentScriptWhen: "start"
+});
